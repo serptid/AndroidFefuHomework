7d74b59d91912983bf48e9c94794778c563238f0
sergay <psi200523@gmail.com>
2025-12-26 16:02:43 +0900

    Revert "Merge pull request #2 from serptid/hw2"
    This reverts commit ae217ff6774a6178859fe9f7e788b6531d3c9f9a, reversing
    changes made to 29805f548deae7d62edd41188f8380578e49c265.

--- HW2/app/src/main/java/com/example/hw2/domain/SplitModels.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hw2.domain
-
-data class SplitResult(
-    val id: String,
-    val tip: Double,
-    val totalWithTip: Double,
-    val perPerson: Double
-)
--- HW2/app/src/main/java/com/example/hw2/ui/InputUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hw2.ui
-
-data class InputUiState(
-    val total: String = "",
-    val people: String = "",
-    val totalError: String? = null,
-    val peopleError: String? = null
-)
