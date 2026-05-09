2339b02151569d1565344f86a2d35e4d2fc54312
Серёжа <111640587+serptid@users.noreply.github.com>
2026-05-09 22:41:38 +1000

    Delete HW1 directory

--- HW1/app/src/main/java/com/example/quiz/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.example.quiz.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val BackgroundDark = Color(0xFF121212)
-val SurfaceDark = Color(0xFF1E1E1E)
-val Primary = Color(0xFFBB86FC)
